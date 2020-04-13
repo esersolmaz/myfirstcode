44077c129c4201c82a3d04afa35ce06e9a87365f
esersolmaz@gmail.com <esersolmaz@gmail.com>
2020-04-13 21:54:40 +0300

    covid19 project add

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print ("""
+1. Toplama
+2. Cikarma
+3. Carpma
+4. Bolme
+""")
+a = int(input("birinci sayi:"))
+b = int(input("ikinci sayi:"))
+islem = input("islem giriniz:")
+
+if (islem == 1):
+    print ("{} ile {} in toplmami {} dir.".format(a,b,a + b))
+elif (islem == 2):
+    print ("{} ile {} in cikarmasi {} dir.".format(a,b,a - b))
+elif (islem == 3):
+    print ("{} ile {} in carpimi {} dir.".format(a,b,a * b))
+elif (islem == 4):
+    print ("{} ile {} in bolmesi {} dir.".format(a,b,a / b))
+else:
+    print("gecersiz islem.")
